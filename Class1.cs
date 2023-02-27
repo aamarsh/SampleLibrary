a299c94a9099e80202f4676651d507d32c653ef1
Ana Marsh <anamarsh@microsoft.com>
2023-02-27 15:36:37 -0800

    conflict

--- Class1.cs
+++ Class1.cs
@@ -11,6 +11,7 @@ namespace MathLibrary
         // another comment
         //adding comment
         // sample library comment
+        // yppppp
         public float Substract(float a, float b)
         {
             return a - b;
